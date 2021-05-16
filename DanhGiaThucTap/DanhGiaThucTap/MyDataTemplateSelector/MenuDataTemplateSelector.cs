4bdb5070b1a5e2e41efc17f4b3412cee69997c30
hunghakma <hunghakma@gmail.com>
2021-05-17 03:58:02 +0700

    fix

--- DanhGiaThucTap/DanhGiaThucTap/MyDataTemplateSelector/MenuDataTemplateSelector.cs
+++ DanhGiaThucTap/DanhGiaThucTap/MyDataTemplateSelector/MenuDataTemplateSelector.cs
@@ -9,17 +9,12 @@ namespace DanhGiaThucTap.MyDataTemplateSelector
     class MenuDataTemplateSelector : DataTemplateSelector
     {
         public DataTemplate MenuTemplate { get; set; }
-        public DataTemplate ItemTemplate { get; set; }
-        public DataTemplate DropdownTemplete { get; set; }
+        public DataTemplate ItemTemplate { get; set; }      
         protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
         {
             if (((MenuModel)item).Title.Equals("Thị trường") || ((MenuModel)item).Title.Equals("Giao dịch") || ((MenuModel)item).Title.Equals("Trợ giúp"))
             {
                 return MenuTemplate;
-            }
-            else if(((MenuModel)item).Title.Equals("Báo cáo giao dịch"))
-            {
-                return DropdownTemplete;
             }
             else
             {
