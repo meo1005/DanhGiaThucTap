13eb941a8a3fd283014410d93a990b65efad0671
hunghakma <hunghakma@gmail.com>
2021-05-17 17:30:47 +0700

    fix

--- DanhGiaThucTap/DanhGiaThucTap/ViewModel/LenhDieuKhienViewModel.cs
+++ DanhGiaThucTap/DanhGiaThucTap/ViewModel/LenhDieuKhienViewModel.cs
@@ -77,7 +77,7 @@ namespace DanhGiaThucTap.ViewModel
             set { SetProperty(ref _index, value); }
         }
 
-        public List<string> ListDK {get; set; }
+        public List<string> ListDK { get; set; }
 
         private bool _giaIsVisible;
         public bool GiaIsVisible
@@ -187,7 +187,7 @@ namespace DanhGiaThucTap.ViewModel
             FontFmMua = FontAttributes.Bold;
             FontFmBan = FontAttributes.None;
             TextBtnXacNhan = "XÁC NHẬN MUA";
-            ItemGT = new ItemGiaTriModel { GTBannerGia = 1765.5, GTMoCua = 1.196, GTCao = 1.132, GTThap = 128.1, GTTongHD = 20.854, GTKLMo = 27.333, GTDuMua00 = 8, GTDuBan01 = 47, GT01 = 138.2, GT00 = 185.7, GTTran = 152.72, GTTB = 118.8, GTSan = 108.6, TileMua = 10, TileBan = 90, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 1 };
+            ItemGT = new ItemGiaTriModel { GTBannerGia = 1765.5, GTMoCua = 1.196, GTCao = 1.132, GTThap = 128.1, GTTongHD = 20.854, GTKLMo = 27.333, GTDuMua00 = 8, GTDuBan01 = 47, GT01 = 138.2, GT00 = 185.7, GTTran = 1952.72, GTTB = 118.8, GTSan = 108.6, TileMua = 10, TileBan = 90, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 1 };
             ItemListNavigation = "VN30F2106";
             EntryGiaDK = ItemGT.GTBannerGia.ToString();
             Sum = (double.Parse(EntryGiaDK) + double.Parse(EntryBienTruot)).ToString();
@@ -276,31 +276,55 @@ namespace DanhGiaThucTap.ViewModel
                 {
                     if (Index == 0)
                     {
-                        EntryGiaDK = (double.Parse(EntryGiaDK) + 0.1).ToString();
-                    }
-                    else if (Index == 1)
-                    {
-                        if (double.Parse(EntryGiaDK) >= ItemGT.GTBannerGia)
+                        if (double.Parse(EntryGiaDK) > ItemGT.GTTran || double.Parse(EntryGiaDK) < ItemGT.GTSan)
                         {
-                            App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện phải nhỏ hơn giá thị trường", "OK");
+                            App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                            EntryGiaDK = ItemGT.GTBannerGia.ToString();
                         }
                         else
                         {
                             EntryGiaDK = (double.Parse(EntryGiaDK) + 0.1).ToString();
                         }
                     }
-                    //dấu cộng trường giá điều kiện với trường hợp mã OCO
-                    else if (Index == 2)
+                    else if (Index == 1)
                     {
-                        if (!SumLai.Equals(""))
+                        if (double.Parse(EntryGiaDK) > ItemGT.GTTran || double.Parse(EntryGiaDK) < ItemGT.GTSan)
                         {
-                            if (double.Parse(EntryGiaDK) >= double.Parse(SumLai) && TextBtnXacNhan.Equals("XÁC NHẬN BÁN"))
+                            App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                            EntryGiaDK = ItemGT.GTBannerGia.ToString();
+                        }
+                        else
+                        {
+                            if (double.Parse(EntryGiaDK) >= ItemGT.GTBannerGia)
                             {
-                                App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện phải nhỏ hơn giá chốt lãi", "OK");
+                                App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện phải nhỏ hơn giá thị trường", "OK");
                             }
                             else
                             {
                                 EntryGiaDK = (double.Parse(EntryGiaDK) + 0.1).ToString();
+                            }
+                        }
+                    }
+                    //dấu cộng trường giá điều kiện với trường hợp mã OCO
+                    else if (Index == 2)
+                    {
+                        if (double.Parse(EntryGiaDK) > ItemGT.GTTran || double.Parse(EntryGiaDK) < ItemGT.GTSan)
+                        {
+                            App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                            EntryGiaDK = ItemGT.GTTB.ToString();
+                        }
+                        else
+                        {
+                            if (!SumLai.Equals(""))
+                            {
+                                if (double.Parse(EntryGiaDK) >= double.Parse(SumLai) && TextBtnXacNhan.Equals("XÁC NHẬN BÁN"))
+                                {
+                                    App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện phải nhỏ hơn giá chốt lãi", "OK");
+                                }
+                                else
+                                {
+                                    EntryGiaDK = (double.Parse(EntryGiaDK) + 0.1).ToString();
+                                }
                             }
                         }
                     }
@@ -349,13 +373,21 @@ namespace DanhGiaThucTap.ViewModel
             {
                 if (Checknegative(EntryGia))
                 {
-                    if (double.Parse(EntryGia) >= ItemGT.GTTran)
+                    if (double.Parse(EntryGia) > ItemGT.GTTran || double.Parse(EntryGia) < ItemGT.GTSan)
                     {
-                        App.Current.MainPage.DisplayAlert("Thông báo", "Giá không được nhỏ hơn giá trần", "OK");
+                        App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                        EntryGia = ItemGT.GTTB.ToString();
                     }
                     else
                     {
-                        EntryGia = (double.Parse(EntryGia) + 0.1).ToString();
+                        if (double.Parse(EntryGia) >= ItemGT.GTTran)
+                        {
+                            App.Current.MainPage.DisplayAlert("Thông báo", "Giá không được cao hơn giá trần", "OK");
+                        }
+                        else
+                        {
+                            EntryGia = (double.Parse(EntryGia) + 0.1).ToString();
+                        }
                     }
                 }
                 else
@@ -428,8 +460,6 @@ namespace DanhGiaThucTap.ViewModel
             {
                 if (Checknegative(EntryGiaDK))
                 {
-
-
                     if (EntryGiaDK.Equals("0"))
                     {
                         EntryGiaDK = "0";
@@ -438,31 +468,55 @@ namespace DanhGiaThucTap.ViewModel
                     {
                         if (Index == 1)
                         {
-                            EntryGiaDK = (double.Parse(EntryGiaDK) - 0.1).ToString();
-                        }
-                        else if (Index == 0)
-                        {
-                            if (double.Parse(EntryGiaDK) <= ItemGT.GTBannerGia)
+                            if(double.Parse(EntryGiaDK)>ItemGT.GTTran|| double.Parse(EntryGiaDK) < ItemGT.GTSan)
                             {
-                                App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện phải lớn hơn giá thị trường", "OK");
+                                App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                                EntryGiaDK = ItemGT.GTBannerGia.ToString();
                             }
                             else
                             {
                                 EntryGiaDK = (double.Parse(EntryGiaDK) - 0.1).ToString();
                             }
                         }
-                        // dấu trừ trường giá điều kiện khi lệnh OCO 
-                        else if (Index == 2)
+                        else if (Index == 0)
                         {
-                            if (!SumLai.Equals(""))
+                            if (double.Parse(EntryGiaDK) > ItemGT.GTTran || double.Parse(EntryGiaDK) < ItemGT.GTSan)
                             {
-                                if (double.Parse(EntryGiaDK) <= double.Parse(SumLai) && TextBtnXacNhan.Equals("XÁC NHẬN MUA"))
+                                App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                                EntryGiaDK = ItemGT.GTBannerGia.ToString();
+                            }
+                            else
+                            {
+                                if (double.Parse(EntryGiaDK) <= ItemGT.GTBannerGia)
                                 {
-                                    App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện phải lớn hơn giá chốt lãi", "OK");
+                                    App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện phải lớn hơn giá thị trường", "OK");
                                 }
                                 else
                                 {
                                     EntryGiaDK = (double.Parse(EntryGiaDK) - 0.1).ToString();
+                                }
+                            }
+                        }
+                        // dấu trừ trường giá điều kiện khi lệnh OCO 
+                        else if (Index == 2)
+                        {
+                            if(double.Parse(EntryGiaDK)>ItemGT.GTTran|| double.Parse(EntryGiaDK) < ItemGT.GTSan)
+                            {
+                                App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                                EntryGiaDK = ItemGT.GTTB.ToString();
+                            }
+                            else
+                            {
+                                if (!SumLai.Equals(""))
+                                {
+                                    if (double.Parse(EntryGiaDK) <= double.Parse(SumLai) && TextBtnXacNhan.Equals("XÁC NHẬN MUA"))
+                                    {
+                                        App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện phải lớn hơn giá chốt lãi", "OK");
+                                    }
+                                    else
+                                    {
+                                        EntryGiaDK = (double.Parse(EntryGiaDK) - 0.1).ToString();
+                                    }
                                 }
                             }
                         }
@@ -512,13 +566,21 @@ namespace DanhGiaThucTap.ViewModel
             {
                 if (Checknegative(EntryGia))
                 {
-                    if (double.Parse(EntryGia) <= ItemGT.GTSan)
+                    if (double.Parse(EntryGia) > ItemGT.GTTran || double.Parse(EntryGia) < ItemGT.GTSan)
                     {
-                        App.Current.MainPage.DisplayAlert("Thông báo", "Giá phải có giá trị lớn hơn giá sàn", "OK");
+                        App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                        EntryGia = ItemGT.GTTB.ToString();
                     }
                     else
                     {
-                        EntryGia = (double.Parse(EntryGia) - 0.1).ToString();
+                        if (double.Parse(EntryGia) <= ItemGT.GTSan)
+                        {
+                            App.Current.MainPage.DisplayAlert("Thông báo", "Giá phải có giá trị lớn hơn giá sàn", "OK");
+                        }
+                        else
+                        {
+                            EntryGia = (double.Parse(EntryGia) - 0.1).ToString();
+                        }
                     }
                 }
                 else
@@ -551,19 +613,19 @@ namespace DanhGiaThucTap.ViewModel
             ListOnNavigationIsVisible = false;
             if (item.ID == 0)
             {
-                ItemGT = new ItemGiaTriModel { GTBannerGia = 1358.5, GTMoCua = 1.191, GTCao = 1.192, GTThap = 1188.2, GTTongHD = 20.82, GTKLMo = 27.33, GTDuMua00 = 12, GTDuBan01 = 30, GT01 = 181.9, GT00 = 118.8, GTTran = 172.32, GTTB = 125.18, GTSan = 105.6, TileMua = 23, TileBan = 77, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 2 };
+                ItemGT = new ItemGiaTriModel { GTBannerGia = 1358.5, GTMoCua = 1.191, GTCao = 1.192, GTThap = 1188.2, GTTongHD = 20.82, GTKLMo = 27.33, GTDuMua00 = 12, GTDuBan01 = 30, GT01 = 181.9, GT00 = 118.8, GTTran = 1772.32, GTTB = 125.18, GTSan = 105.6, TileMua = 23, TileBan = 77, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 2 };
             }
             else if (item.ID == 1)
             {
-                ItemGT = new ItemGiaTriModel { GTBannerGia = 1399.1, GTMoCua = 1.131, GTCao = 1191, GTThap = 1128.3, GTTongHD = 20.242, GTKLMo = 27.313, GTDuMua00 = 7, GTDuBan01 = 21, GT01 = 188.3, GT00 = 138.6, GTTran = 147.93, GTTB = 128.88, GTSan = 115.6, TileMua = 50, TileBan = 50, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 3 };
+                ItemGT = new ItemGiaTriModel { GTBannerGia = 1399.1, GTMoCua = 1.131, GTCao = 1191, GTThap = 1128.3, GTTongHD = 20.242, GTKLMo = 27.313, GTDuMua00 = 7, GTDuBan01 = 21, GT01 = 188.3, GT00 = 138.6, GTTran = 1847.93, GTTB = 128.88, GTSan = 115.6, TileMua = 50, TileBan = 50, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 3 };
             }
             else if (item.ID == 2)
             {
-                ItemGT = new ItemGiaTriModel { GTBannerGia = 1354.9, GTMoCua = 1.191, GTCao = 1.196, GTThap = 1178.7, GTTongHD = 20.832, GTKLMo = 27.383, GTDuMua00 = 3, GTDuBan01 = 17, GT01 = 118.8, GT00 = 168.7, GTTran = 111.214, GTTB = 108.8, GTSan = 96.6, TileMua = 59.26, TileBan = 40.74, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 4 };
+                ItemGT = new ItemGiaTriModel { GTBannerGia = 1354.9, GTMoCua = 1.191, GTCao = 1.196, GTThap = 1178.7, GTTongHD = 20.832, GTKLMo = 27.383, GTDuMua00 = 3, GTDuBan01 = 17, GT01 = 118.8, GT00 = 168.7, GTTran = 1911.214, GTTB = 108.8, GTSan = 96.6, TileMua = 59.26, TileBan = 40.74, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 4 };
             }
             else
             {
-                ItemGT = new ItemGiaTriModel { GTBannerGia = 1765.5, GTMoCua = 1.196, GTCao = 1.132, GTThap = 128.1, GTTongHD = 20.854, GTKLMo = 27.333, GTDuMua00 = 8, GTDuBan01 = 47, GT01 = 138.2, GT00 = 185.7, GTTran = 152.72, GTTB = 148.8, GTSan = 108.6, TileMua = 10, TileBan = 90, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 5 };
+                ItemGT = new ItemGiaTriModel { GTBannerGia = 1765.5, GTMoCua = 1.196, GTCao = 1.132, GTThap = 128.1, GTTongHD = 20.854, GTKLMo = 27.333, GTDuMua00 = 8, GTDuBan01 = 47, GT01 = 138.2, GT00 = 185.7, GTTran = 1952.72, GTTB = 148.8, GTSan = 108.6, TileMua = 10, TileBan = 90, GT10 = 132.64, GT11 = 137.72, GT20 = 188.123, GT21 = 135.32, GTDuBan11 = 2, GTDuBan21 = 23, GTDuMua10 = 12, GTDuMua20 = 34, KLMax = 5 };
             }
             if (Index == 0 || Index == 1)
             {
@@ -805,6 +867,7 @@ namespace DanhGiaThucTap.ViewModel
             ListOnNavigation.Add(new ItemListNavigationModel { ID = 6, Ma = "GB10F2109" });
             ListOnNavigation.Add(new ItemListNavigationModel { ID = 7, Ma = "GB10F2106" });
         }
+        //check số âm, check null
         private bool Checknegative(string n)
         {
             if (n == "")
@@ -821,6 +884,19 @@ namespace DanhGiaThucTap.ViewModel
                 {
                     return true;
                 }
+            }
+        }
+        //check khoang giá trị
+        private bool CheckValue(string n)
+        {
+            if (double.Parse(n) > ItemGT.GTTran || double.Parse(n) < ItemGT.GTSan)
+            {
+                App.Current.MainPage.DisplayAlert("Thông báo", "Giá điều kiện không được nằm ngoài biên độ trần sàn", "OK");
+                return false;
+            }
+            else
+            {
+                return true;
             }
         }
     }
